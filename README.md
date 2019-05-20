f6d5e6b5a327f47382aabebdfc78a4fc4aaff526
markdoma <48811068+markdoma@users.noreply.github.com>
2019-05-20 18:23:49 +0800

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# MovieFinder
-Created with CodeSandbox
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = props => {
+  return (
+    <header className="App Header">
+      <h2>{props.text}</h2>
+    </header>
+  );
+};
+
+export default Header;
